bdf7785509b17dd8e35329d4a2a05c1419a6b4fb
sly471 <gaban421@hotmail.com>
2018-12-07 09:31:59 -0400

    added comments to the file "colorCalc_and_Randomizer.js"

--- scripts/colorCalc_and_Randomizer.js
+++ scripts/colorCalc_and_Randomizer.js
@@ -1,3 +1,8 @@
+// input: any dropoff selected
+// output: an assign color to the dropoff for easy reading of the weight of dp when added to the route
+// green:  between 0-29 minutes added
+// yellow: between 30-59 minutes added
+// red:    more or equal than 60 minutes added 
 function colorCalculator(dropoff){
     if(dropoff.weight >= 60){
         return '#ff0000';
@@ -11,11 +16,19 @@ function colorCalculator(dropoff){
 };
 
 // Returns an integer between min(included) & max(excluded)
+// input: minimum number of a range of numbers & a maximum number of a range of numbers
+// output: a random integer that falls in the range of the input numbers
 function getRndInteger(min, max) {return Math.floor(Math.random() * (max - min) ) + min;};
 
 // Returns a float between min(included) & max(excluded)
+// input: minimum number of a range of numbers & a maximum number of a range of numbers
+// output: a random float that falls in the range of the input numbers
 function getRndFloat(min, max) {return Math.random() * (max - min)  + min;};
 
+
+// Test function used for creating random dropoff locations
+// input: list of dropoffs available
+// output: same list with an extra dropoff created randomly
 function createRndDropoff(dropoffLocations){
     for (var i=0; i<50; i++){
         x= getRndFloat(-67.13664550786771, -65.70716270963167)
